4b602e75ecb3ad87cbff679abc9767620ebc2d1b
JuliaSteffen <julia_steffen@estudante.sesisenai.org.br>
2025-10-30 16:35:19 -0300

    gateway

--- app/gateway.php
+++ app/gateway.php
@@ -1,1 +1,21 @@
-<php
+<?php
+    require_once __DIR__ . '/classes/Conexao.php';
+
+    $acao = $_GET['acao'];
+    
+
+    if($acao == 'conexao'){
+        $conexao = new Conexao();
+    }else if ($acao == 'cadastrar'){
+
+        //$_POST[]
+        $usuario = new Usuario();
+        $usuario->inserir();
+    }
+   
+
+    
+    
+
+
+    
